ac4a8ac39107851a1ab86c958190af61dc23f0bc
FreeJaguar <71263111+FreeJaguar@users.noreply.github.com>
2025-06-29 18:38:13 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,4 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'לוח מידע דיגיטלי - בית כנסת',
@@ -15,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="he">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
